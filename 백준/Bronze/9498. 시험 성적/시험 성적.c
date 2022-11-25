50a312f87443d81e0854f43a99dbbf5e2ad0079a
jjk23 <37400378+jjk23@users.noreply.github.com>
2022-11-25 10:24:30 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>89)
+	{
+		printf("A");
+	}
+	else if(a>79)
+	{
+		printf("B");
+	}
+	else if(a>69)
+	{
+		printf("C");
+	}
+	else if(a>59)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("F");
+	}
+ } 
