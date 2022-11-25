20d0d5e8c4da6bc8fd01c8bc72bf102cd3f379da
jjk23 <37400378+jjk23@users.noreply.github.com>
2022-11-25 10:23:59 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+main()
+{
+    printf("         ,r'\"7");
+    printf("\nr`-_   ,'  ,/");
+    printf("\n \\. \". L_r'");
+    printf("\n   `~\\/");
+    printf("\n      |");
+    printf("\n      |\n");
+    return 0;
+}
