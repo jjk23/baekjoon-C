351a0c8b5cf7ed2486a4bcb7bf7fb3917f515573
jjk23 <37400378+jjk23@users.noreply.github.com>
2022-12-04 21:59:15 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char* arr) {
+    int len = strlen(arr), i;
+    for (i = 0; i < len / 2; i++) {
+        char temp = arr[i];
+        arr[i] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+    }
+}
+
+int main() {
+    char A[10002]={NULL,}, B[10002]={NULL,}, r[10003]={NULL,}, carry = 0, sum;
+    int i, len;
+    scanf("%s %s", A, B);
+    reverse(A);
+    reverse(B);
+    len = strlen(A) > strlen(B) ? strlen(A) : strlen(B);
+    for (i = 0; i < len; i++) {
+        sum = A[i] - '0' + B[i] - '0' + carry;
+        if (sum < 0)
+            sum += '0';
+        if (sum > 9)
+            carry = 1;
+        else
+            carry = 0;
+        r[i] = sum % 10 + '0';
+    }
+    if (carry == 1)
+        r[len] = '1';
+    reverse(r);
+    printf("%s\n", r);
+
+    return 0;
+}
