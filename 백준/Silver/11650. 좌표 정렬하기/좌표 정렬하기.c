0930eaf5c44d38db0b1d91b8c92e5c90bfc30708
jjk23 <37400378+jjk23@users.noreply.github.com>
2022-12-22 10:30:14 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 68 ms, Memory: 2676 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x;
+    int y;
+} coord; //coord형의 구조체 선언
+
+int compare(const void* a, const void* b)
+{
+    coord A = *(coord*)a; 
+    coord B = *(coord*)b;
+    if (A.x > B.x) 
+        return 1;
+    else if (A.x == B.x) 
+    {
+        if (A.y > B.y) 
+            return 1;
+        else
+            return -1; 
+    }
+    return -1; 
+}
+coord arr[100000]; 
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    i = 0;
+    
+    while (i < n)
+    {
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+        i++;
+    }
+    qsort(arr, n, sizeof(coord), compare); 
+    i = 0;
+    while (i < n)
+    {
+        printf("%d %d\n", arr[i].x, arr[i].y); 
+        i++;
+    }
+}
